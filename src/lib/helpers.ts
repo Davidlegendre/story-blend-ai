25cdeb405d21e20ca70dd1c79fc414b04ab44911
Davidlegendre <legendre2013@hotmail.com>
2024-08-07 23:54:26 -0500

    intento

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -11,7 +11,8 @@ export const getResponse = (ResponseModel: ResponseModel<any>) => {
 };
 
 export const getSession = async () => {
-  const user = (await supabase.auth.getSession()).data.session;
+  const session = await supabase.auth.getSession()
+  const user = session.data.session
   return user ?? null;
 };
 
