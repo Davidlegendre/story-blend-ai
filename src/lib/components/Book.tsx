e5c11e4a67912cbc92ef67ce25ef4908921a4a81
Davidlegendre <legendre2013@hotmail.com>
2024-08-25 00:51:04 -0500

    book update

--- src/lib/components/Book.tsx
+++ src/lib/components/Book.tsx
@@ -1,26 +1,67 @@
 import styles from "./css/Book.module.css";
 
-export function Book() {
+export enum typeBook {
+  simple = "simple",
+  portada = "portada",
+  ambos = "ambos",
+}
+
+interface Props {
+  text: string;
+  showLogo?: boolean;
+  type?: typeBook;
+  color?: string;
+  urlImage?: string;
+  urlLogo?: string;
+}
+
+export function Book({
+  text,
+  showLogo = true,
+  type = typeBook.ambos,
+  color = undefined,
+  urlImage = undefined,
+  urlLogo = "https://img.icons8.com/?size=100&id=93961&format=png&color=000000"
+}: Props) {
+  const portadaStyle = [styles.portada_book];
+  switch (type) {
+    case typeBook.simple:
+      portadaStyle.push(styles.expand_part_top);
+      break;
+    case typeBook.portada:
+      portadaStyle.push(styles.expand_part_title);
+      break;
+  }
+
+  const getStyles = (style: string[]) => {
+    return style.join(" ");
+  };
+
   return (
-    <div className={styles.book}>
-      <div className={styles.back_book}></div>
-      <div className={styles.pages_book}></div>
-      <div className={styles.content_book}>
-        <div className={styles.portada_book}>
-          <div className={styles.top_book}>
-            <p>imagen o lo que sea</p>
-            <div className={styles.book_lomo1}></div>
+    <div className={styles.theme}>
+      <div className={styles.book}>
+        <div className={styles.back_book}></div>
+        <div className={styles.pages_book}></div>
+        <div className={styles.content_book}>
+          <div className={getStyles(portadaStyle)}>
+            <div className={styles.top_book} style={{ backgroundColor: color }}>
+              {urlImage ? <img src={urlImage} alt="portada-book"></img> : ""}
+            </div>
+            <div className={styles.title_book}>
+              <p>{text}</p>
+              {showLogo ? (
+                <img
+                  className={styles.vercel_icon}
+                  width={24}
+                  src={urlLogo}
+                  alt="logo"
+                ></img>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
-          <div className={styles.title_book}>
-            <div className={styles.book_lomo2}></div>
-            <p>TITULO DEL LIBRO</p>
-            <img
-              className={styles.vercel_icon}
-              width={18}
-              src="https://img.icons8.com/?size=100&id=93961&format=png&color=000000"
-              alt="logo"
-            ></img>
-          </div>
+          <div className={styles.book_lomo}></div>
         </div>
       </div>
     </div>
